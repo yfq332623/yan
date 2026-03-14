61016edc076fa4996b1a38e671661e578acfb252
颜凡棋 <1729583616@qq.com>
2026-03-14 14:14:48 +0800

    chore: 整理目录结构，清理冗余文件

---
+++ build/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/yanfanqi/yan/myweb/src/connection_pool.cpp" "CMakeFiles/server.dir/src/connection_pool.cpp.o" "gcc" "CMakeFiles/server.dir/src/connection_pool.cpp.o.d"
+  "/home/yanfanqi/yan/myweb/src/http.cpp" "CMakeFiles/server.dir/src/http.cpp.o" "gcc" "CMakeFiles/server.dir/src/http.cpp.o.d"
+  "/home/yanfanqi/yan/myweb/src/log.cpp" "CMakeFiles/server.dir/src/log.cpp.o" "gcc" "CMakeFiles/server.dir/src/log.cpp.o.d"
+  "/home/yanfanqi/yan/myweb/src/main.cpp" "CMakeFiles/server.dir/src/main.cpp.o" "gcc" "CMakeFiles/server.dir/src/main.cpp.o.d"
+  "/home/yanfanqi/yan/myweb/src/util_timer.cpp" "CMakeFiles/server.dir/src/util_timer.cpp.o" "gcc" "CMakeFiles/server.dir/src/util_timer.cpp.o.d"
+  "/home/yanfanqi/yan/myweb/src/webServer.cpp" "CMakeFiles/server.dir/src/webServer.cpp.o" "gcc" "CMakeFiles/server.dir/src/webServer.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/src/connection_pool.cpp.o"
+  "CMakeFiles/server.dir/src/connection_pool.cpp.o.d"
+  "CMakeFiles/server.dir/src/http.cpp.o"
+  "CMakeFiles/server.dir/src/http.cpp.o.d"
+  "CMakeFiles/server.dir/src/log.cpp.o"
+  "CMakeFiles/server.dir/src/log.cpp.o.d"
+  "CMakeFiles/server.dir/src/main.cpp.o"
+  "CMakeFiles/server.dir/src/main.cpp.o.d"
+  "CMakeFiles/server.dir/src/util_timer.cpp.o"
+  "CMakeFiles/server.dir/src/util_timer.cpp.o.d"
+  "CMakeFiles/server.dir/src/webServer.cpp.o"
+  "CMakeFiles/server.dir/src/webServer.cpp.o.d"
+  "server"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
